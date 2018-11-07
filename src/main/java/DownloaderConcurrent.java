79cd5cdad370d2c581045b7495eaccc907abaeb8
Viktor Burka <viktor.burka@turner.com>
2018-11-07 18:04:31 -0500

    Removed stub files

--- src/main/java/DownloaderConcurrent.java
+++
@@ -1,9 +0,0 @@
-import java.io.OutputStream;
-import java.util.Map;
-
-public class DownloaderConcurrent implements Downloader {
-
-    @Override
-    public void download(String url, Map<String,String> options, Receiver rc, OutputStream writer)  throws Exception {
-    }
-}
